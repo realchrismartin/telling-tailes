82affab1d7d0d7ddd9913df566b1b16ef6459709
CM <omegajms@gmail.com>
2022-03-31 19:17:20 -0400

    TT: Create initial activities, layouts, and package structure

---
+++ app/src/main/java/com/telling/tailes/activity/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.telling.tailes.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.telling.tailes.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.feedTempButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToFeed();
+            }
+        });
+
+        findViewById(R.id.createTempButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToCreate();
+            }
+        });
+
+    }
+
+    public void goToFeed()
+    {
+        Intent intent = new Intent(this,StoryFeedActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void goToCreate()
+    {
+        Intent intent = new Intent(this,CreateStoryActivity.class);
+
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/telling/tailes/activity/PublishStoryActivity.java
+++ app/src/main/java/com/telling/tailes/activity/PublishStoryActivity.java
@@ -1,14 +1,16 @@
-package com.telling.tailes;
+package com.telling.tailes.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.telling.tailes.R;
+
+public class PublishStoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_publish_story);
     }
 }
