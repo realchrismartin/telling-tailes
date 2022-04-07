7ecda8bdf36044af2771b23aa2c8327838211c05
Will Cohen <whysgai@gmail.com>
2022-04-07 08:43:47 -0400

    TT-47 feat: Create addLove() method shell

---
+++ app/src/main/java/com/telling/tailes/util/FBUtils.java
@@ -0,0 +1,10 @@
+package com.telling.tailes.util;
+
+import android.util.Log;
+
+public class FBUtils {
+
+    public static void addLove() {
+        Log.d("addLove", "adding love to FB");
+    }
+}
