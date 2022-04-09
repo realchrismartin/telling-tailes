1dc0885d4a3bb7d361a141ac9a1d3a2204ac8d8b
CM <omegajms@gmail.com>
2022-04-09 14:12:21 -0400

    TT-16: add login methods

--- app/src/main/java/com/telling/tailes/activity/LoginActivity.java
+++ app/src/main/java/com/telling/tailes/activity/LoginActivity.java
@@ -1,65 +1,77 @@
 package com.telling.tailes.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.telling.tailes.R;
+import com.telling.tailes.util.AuthUtils;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView usernameEntryView;
+    private TextView passwordEntryView;
+    private Toast loginToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        findViewById(R.id.feedTempButton).setOnClickListener(new View.OnClickListener() {
+        //Set views
+        usernameEntryView = findViewById(R.id.enterUsernameView);
+        passwordEntryView = findViewById(R.id.enterPasswordView);
+
+        //Create toast
+        loginToast = Toast.makeText(getApplicationContext(),"",Toast.LENGTH_SHORT);
+
+        //Set listeners
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToFeed();
+                login();
             }
         });
 
-        findViewById(R.id.createTempButton).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.createAccountNavButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToCreate();
             }
         });
 
-        findViewById(R.id.testMenuButton).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {goToHTest();}
-        });
-
     }
 
-
-    private void goToHTest() {
-        Intent intent = new Intent(this, HTestActivity.class);
-        startActivity(intent);
-    }
-
-//    private void goToH2Test() {
-//        Intent intent = new Intent(this, HTwoTestActivity.class);
-//        startActivity(intent);
-//    }
-
-    public void goToFeed()
+    /*
+        Attempt a login to the app
+        If the login is successful, redirect to the feed
+     */
+    public void login()
     {
-        Intent intent = new Intent(this,StoryFeedActivity.class);
 
-        startActivity(intent);
+        String errorResult = AuthUtils.logInUser(usernameEntryView.getText().toString(),passwordEntryView.getText().toString());
+
+        if(errorResult.length() <= 0)
+        {
+            Intent intent = new Intent(this,StoryFeedActivity.class);
+            startActivity(intent);
+            return;
+        }
+
+        loginToast.setText(errorResult);
+        loginToast.show();
     }
 
+    /*
+        Navigate to the Create Account activity
+     */
     public void goToCreate()
     {
-        Intent intent = new Intent(this,CreateStoryActivity.class);
+        Intent intent = new Intent(this,CreateAccountActivity.class);
 
         startActivity(intent);
     }
--- app/src/main/java/com/telling/tailes/util/AuthUtils.java
+++ app/src/main/java/com/telling/tailes/util/AuthUtils.java
@@ -2,9 +2,25 @@ package com.telling.tailes.util;
 
 public class AuthUtils {
 
-    //TODO
-    public static String getLoggedInUserID()
+    /*
+        Return the name of the logged in user
+        If a user is not logged in, return a blank string
+     */
+    public static String getLoggedInUserID() {
+        return "authortest"; //TODO
+    }
+
+    /*
+        Return true if a user is logged in, false otherwise
+     */
+    public static boolean userIsLoggedIn() {
+        return false; //TODO
+    }
+
+    //Attempts to log in as the specified user
+    //Returns a zero length string if successful, otherwise returns an error
+    public static String logInUser(String username, String password)
     {
-        return "authortest";
+        return "There was some error"; //TODO
     }
 }
