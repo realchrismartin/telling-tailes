bda484ca1a035e1c13657c9572dbbab2131c8e45
CM <omegajms@gmail.com>
2022-04-25 10:46:27 -0400

    TT-118: add creative controls

--- app/src/main/java/com/telling/tailes/activity/CreateStoryActivity.java
+++ app/src/main/java/com/telling/tailes/activity/CreateStoryActivity.java
@@ -62,6 +62,9 @@ public class CreateStoryActivity extends AppCompatActivity {
     private ActivityResultLauncher<Intent> launcher;
 
     private SeekBar lengthSeekBar;
+    private SeekBar temperatureSeekBar;
+    private SeekBar presenceSeekBar;
+    private SeekBar frequencySeekBar;
     private TextView promptView;
     private ProgressBar loadingWheel;
     private Toast toast;
@@ -85,6 +88,10 @@ public class CreateStoryActivity extends AppCompatActivity {
 
         //Set up views
         lengthSeekBar = findViewById(R.id.lengthSlider);
+        temperatureSeekBar = findViewById(R.id.temperatureSlider);
+        presenceSeekBar = findViewById(R.id.presenceSlider);
+        frequencySeekBar = findViewById(R.id.frequencySlider);
+
         promptView = findViewById(R.id.promptView);
         loadingWheel = findViewById(R.id.storyCreateLoadingWheel);
 
@@ -100,6 +107,15 @@ public class CreateStoryActivity extends AppCompatActivity {
         //Set seekbar min and max
         lengthSeekBar.setMin(lengthMin);
         lengthSeekBar.setMax(lengthMax);
+
+        temperatureSeekBar.setMin(0);
+        temperatureSeekBar.setMax(100);
+
+        presenceSeekBar.setMin(0);
+        presenceSeekBar.setMax(100);
+
+        frequencySeekBar.setMin(0);
+        frequencySeekBar.setMax(100);
 
         //Set up background executor for handling web request threads
         backgroundTaskExecutor = Executors.newFixedThreadPool(2);
@@ -305,8 +321,12 @@ public class CreateStoryActivity extends AppCompatActivity {
             @Override
             public void run() {
 
+                double temperature = 0.9 - temperatureSeekBar.getProgress() * .002; //Inverted from selection - left is creative, attenuated to be .1x as strong
+                double presence = presenceSeekBar.getProgress() / 50.0; //Only allows positive values
+                double frequency = frequencySeekBar.getProgress() / 50.0;
+
                 //Ask GPT to complete the prompt
-                String story = GPTUtils.getStory(getApplicationContext(), promptView.getText().toString().trim(), lengthSeekBar.getProgress());
+                String story = GPTUtils.getStory(getApplicationContext(), promptView.getText().toString().trim(), lengthSeekBar.getProgress(),temperature,presence,frequency);
                 int resultCode = story.length() <= 0 ? 1 : 0;
 
                 //Set up a bundle
