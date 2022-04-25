2696fc11ac1fbd1e4a8061dc65d08fb65442762d
Will Cohen <whysgai@gmail.com>
2022-04-25 19:40:10 -0400

    TT-95 feat: Refactor author profile dialog fragment for boring buttons

--- app/src/main/java/com/telling/tailes/fragment/AuthorProfileDialogFragment.java
+++ app/src/main/java/com/telling/tailes/fragment/AuthorProfileDialogFragment.java
@@ -11,6 +11,7 @@ import android.os.Message;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -18,12 +19,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
 import com.telling.tailes.R;
 import com.telling.tailes.activity.StoryFeedActivity;
 import com.telling.tailes.model.User;
 import com.telling.tailes.util.AuthUtils;
+import com.telling.tailes.util.DrawableUtils;
 import com.telling.tailes.util.FBUtils;
 
 import java.util.concurrent.Executor;
@@ -40,14 +43,14 @@ public class AuthorProfileDialogFragment extends DialogFragment {
     private int followCount;
     private boolean following;
     private String[] menuOptions;
-    private String followOptionText;
-    private String unfollowOptionText;
     private String placeholderUsernameText;
     private String readUserStoriesText;
     private String readOptionText;
     private Toast profileToast;
     private TextView profileUserNameView;
     private ImageView profileButtonView;
+    private Button authorProfileFollowButton;
+    private Button authorProfileStoriesButton;
 
     private Executor backgroundTaskExecutor;
     private Handler backgroundTaskResultHandler;
@@ -60,12 +63,15 @@ public class AuthorProfileDialogFragment extends DialogFragment {
         //Init dialog members
         init();
 
+        //Set up toast
+        profileToast = Toast.makeText(getContext(),"",Toast.LENGTH_SHORT);
+
         //Set up executor
         backgroundTaskExecutor = Executors.newFixedThreadPool(2);
 
         //Set up views
-        final ArrayAdapter<String> arrayAdapterItems = new ArrayAdapter<String>(
-                getContext(), R.layout.author_profile_list_item, menuOptions);
+/*        final ArrayAdapter<String> arrayAdapterItems = new ArrayAdapter<String>(
+                getContext(), R.layout.author_profile_list_item, menuOptions);*/
 
         View content = getLayoutInflater().inflate(R.layout.author_profile_dialog, null);
 
@@ -85,11 +91,20 @@ public class AuthorProfileDialogFragment extends DialogFragment {
         profileUserNameView.setText(authorId);
 
         profileButtonView = content.findViewById(R.id.author_profile_user_profile_image);
-//        profileButtonView.setCompoundDrawablesWithIntrinsicBounds(ContextCompat.getDrawable(getContext(), DrawableUtils.getProfileIconResourceId(profileIcon)),null,null,null);
+        profileButtonView.setImageDrawable(ContextCompat.getDrawable(getContext(), DrawableUtils.getProfileIconResourceId(profileIcon)));
 
-        final ListView items = content.findViewById(R.id.author_profile_options_view);
-        items.setAdapter(arrayAdapterItems);
-        items.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        authorProfileStoriesButton = content.findViewById(R.id.authorProfileStoriesButton);
+        authorProfileStoriesButton.setText(authorId + getString(R.string.author_profile_read_option) );
+
+        String followOptionText = getString(R.string.author_profile_follow_option);
+        String unfollowOptionText = getString(R.string.author_profile_unfollow_option);
+
+        authorProfileFollowButton = content.findViewById(R.id.authorProfileFollowButton);
+        if(following) {
+            authorProfileFollowButton.setText(unfollowOptionText);
+        } else {
+            authorProfileFollowButton.setText(followOptionText);
+        }
 
         //Handle background task interactions with the follow/unfollow toggle
         backgroundTaskResultHandler = new Handler(Looper.getMainLooper()) {
@@ -112,13 +127,11 @@ public class AuthorProfileDialogFragment extends DialogFragment {
                 CheckedTextView checkView = (CheckedTextView) content.findViewById(R.id.author_profile_check_view);
 
                 if(following) {
-                    checkView.setChecked(true);
-                    checkView.setText(unfollowOptionText);
+                    authorProfileFollowButton.setText(unfollowOptionText);
                     profileToast.setText(getText(R.string.author_profile_follow_notification) + " " + authorId);
                     followCount += 1;
                 } else {
-                    checkView.setChecked(false);
-                    checkView.setText(followOptionText);
+                    authorProfileFollowButton.setText(followOptionText);
                     profileToast.setText(getText(R.string.author_profile_unfollow_notification) + " " + authorId);
                     followCount -= 1;
                 }
@@ -129,39 +142,37 @@ public class AuthorProfileDialogFragment extends DialogFragment {
             }
         };
 
-        //Set up item onClick listener for dialog options
-        items.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        authorProfileFollowButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String option = adapterView.getItemAtPosition(i).toString();
+            public void onClick(View view) {
+                String currentUsername = AuthUtils.getLoggedInUserID(getContext());
 
-                if (option.equals(followOptionText) || option.equals(unfollowOptionText)) {
-
-                    String currentUsername = AuthUtils.getLoggedInUserID(getContext());
-
-                    if(currentUsername.equals("")){
-                        return;
-                    }
-
-                    if(currentUsername.equals(authorId)) {
-                        profileToast.setText(R.string.author_profile_follow_self_error);
-                        profileToast.show();
-                        return;
-                    }
-
-                    //Handle following the author if not following self
-                    handleAuthorFollow(authorId);
+                if(currentUsername.equals("")){
                     return;
                 }
 
-                if(option.equals(readOptionText)) {
-                    Intent intent = new Intent(getContext(), StoryFeedActivity.class);
-                    intent.putExtra("feedFilter", "By Author");
-                    intent.putExtra("authorId",authorId);
-                    startActivity(intent);
+                if(currentUsername.equals(authorId)) {
+                    profileToast.setText(R.string.author_profile_follow_self_error);
+                    profileToast.show();
+                    return;
                 }
+
+                //Handle following the author if not following self
+                handleAuthorFollow(authorId);
             }
         });
+
+
+        authorProfileStoriesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), StoryFeedActivity.class);
+                intent.putExtra("feedFilter", "By Author");
+                intent.putExtra("authorId",authorId);
+                startActivity(intent);
+            }
+        });
+
 
         //Create dialog via builder
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -174,16 +185,6 @@ public class AuthorProfileDialogFragment extends DialogFragment {
         Set up the dialog with data from the calling activity
      */
     private void init() {
-
-        //Set up toast
-        profileToast = Toast.makeText(getContext(),"",Toast.LENGTH_SHORT);
-
-        //Set up option text
-        followOptionText = getResources().getString(R.string.author_profile_follow_option);
-        unfollowOptionText = getResources().getString(R.string.author_profile_unfollow_option);
-        readUserStoriesText = getResources().getString(R.string.author_profile_read_option);
-        placeholderUsernameText = getResources().getString(R.string.author_profile_placeholder_username);
-
         //Set member properties from args
         Bundle args = getArguments();
 
@@ -220,38 +221,8 @@ public class AuthorProfileDialogFragment extends DialogFragment {
             following = args.getBoolean("following");
         }
 
-        readOptionText = readUserStoriesText.replace(placeholderUsernameText,authorId);
-
-        //Set menu options (after member data is loaded)
-        menuOptions = updateValues(getResources().getStringArray(R.array.author_profile_options));
     }
-    /*
-        Update the option list to reflect whether the author is followed or not
-     */
-    private String[] updateValues(String[] initialValues) {
-        String[] result = new String[initialValues.length];
 
-        for(int i=0;i<initialValues.length;i++) {
-
-            //Update the follow option if already following this user
-            if(initialValues[i].equals(followOptionText)) {
-                if(following) {
-                    result[i] = unfollowOptionText;
-                    continue;
-                }
-            }
-
-            //Update the read option to show the author's username
-            if(initialValues[i].equals(readUserStoriesText)) {
-               result[i] = readOptionText;
-               continue;
-            }
-
-            result[i]  = initialValues[i];
-        }
-
-        return result;
-    }
 
     /*
         Fragment onClick handler for following or unfollowing an author
