4b4d5e039216643634acd02edaa281ada1f3cc20
mino <reyes.me@northeastern.edu>
2022-04-27 17:33:25 -0400

    TT-127 feat: added button to about page

--- app/src/main/java/com/telling/tailes/activity/AboutActivity.java
+++ app/src/main/java/com/telling/tailes/activity/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.telling.tailes.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +12,15 @@ import com.telling.tailes.R;
 public class AboutActivity extends AppCompatActivity {
 
     private TextView aboutText;
+    private Button tutorialButton;
+
+
     protected void onCreate (Bundle onSavedInstanceState) {
         super.onCreate(onSavedInstanceState);
         setContentView(R.layout.activity_about);
+        tutorialButton = findViewById(R.id.tutorialButton);
+
+
     }
 
 }
