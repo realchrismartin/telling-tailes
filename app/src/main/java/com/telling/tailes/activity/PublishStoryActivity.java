c35a5c7339e4023dba023a933f67585006a63933
CM <omegajms@gmail.com>
2022-04-27 17:44:14 -0400

    TT-123: add spacing to message body

--- app/src/main/java/com/telling/tailes/activity/PublishStoryActivity.java
+++ app/src/main/java/com/telling/tailes/activity/PublishStoryActivity.java
@@ -448,7 +448,7 @@ public class PublishStoryActivity extends AppCompatActivity {
                                        return;
                                    }
 
-                                   String body = user.getUsername() + getString(R.string.message_published_story_body) + ": " + story.getTitle();
+                                   String body = user.getUsername() + " " + getString(R.string.message_published_story_body) + ": " + story.getTitle();
 
                                    FBUtils.sendNotificationToFollowers(getApplicationContext(), user.getUsername(), getString(R.string.message_published_story), body, "", "publish", storyId, new Consumer<Boolean>() {
                                        @Override
