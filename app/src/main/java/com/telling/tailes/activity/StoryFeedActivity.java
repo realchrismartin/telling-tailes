29c327fbcf11a36b4ad2dd678d709d84efacd162
Will Cohen <whysgai@gmail.com>
2022-04-28 20:22:08 -0400

    TT-62 feat: Fully implement loading card

--- app/src/main/java/com/telling/tailes/activity/StoryFeedActivity.java
+++ app/src/main/java/com/telling/tailes/activity/StoryFeedActivity.java
@@ -132,6 +132,7 @@ public class StoryFeedActivity extends AppCompatActivity implements AdapterView.
                 }
 
                 switch(msg.getData().getString("type")) {
+//                      case("thing"): {
                     case("storyData"): {
                         if(msg.getData().getInt("result") != 0) {
                             toast.setText(R.string.generic_error_notification);
@@ -413,11 +414,6 @@ public class StoryFeedActivity extends AppCompatActivity implements AdapterView.
             return;
         }
 
-        Toast.makeText(StoryFeedActivity.this,
-                "Loading more stories",
-                Toast.LENGTH_SHORT)
-                .show();
-
         addLoadingCard();
         loadStoryData();
     }
@@ -566,7 +562,9 @@ public class StoryFeedActivity extends AppCompatActivity implements AdapterView.
 
                 currentFilter = FilterType.get(selection);
                 currentFilter.setAuthorFilter(authorId);
-                refreshStories();
+                if (loadingCardIndex < 0) {
+                    refreshStories();
+                }
                 break;
             }
         }
@@ -679,14 +677,17 @@ public class StoryFeedActivity extends AppCompatActivity implements AdapterView.
             loadingCardIndex = storyCardList.size();
             storyCardList.add(new StoryRviewCard());
             storyRviewAdapter.notifyItemInserted(loadingCardIndex);
+//            storyRviewAdapter.notifyDataSetChanged();
         }
     }
 
     public void removeLoadingCard() {
-        if (storyCardList.size() - 1 == loadingCardIndex) {
-            storyCardList.remove(storyCardList.size() - 1);
-            storyRviewAdapter.notifyItemRemoved(loadingCardIndex);
-            loadingCardIndex = -1;
+        if (loadingCardIndex >= 0) {
+            if (storyCardList.size() - 1 == loadingCardIndex) {
+                storyCardList.remove(storyCardList.size() - 1);
+                storyRviewAdapter.notifyItemRemoved(loadingCardIndex);
+                loadingCardIndex = -1;
+            }
         }
     }
 }
