42df8119c5ee0f00a575550a2eb61296cef462cf
Will Cohen <whysgai@gmail.com>
2022-04-28 21:40:38 -0400

    TT-120 fix: Remove extraneous switch cases

--- app/src/main/java/com/telling/tailes/activity/StoryFeedActivity.java
+++ app/src/main/java/com/telling/tailes/activity/StoryFeedActivity.java
@@ -132,7 +132,6 @@ public class StoryFeedActivity extends AppCompatActivity implements AdapterView.
                 }
 
                 switch(msg.getData().getString("type")) {
-//                      case("thing"): {
                     case("storyData"): {
                         if(msg.getData().getInt("result") != 0) {
                             toast.setText(R.string.generic_error_notification);
@@ -227,9 +226,6 @@ public class StoryFeedActivity extends AppCompatActivity implements AdapterView.
                         authorProfileDialogFragment.setArguments(msg.getData());
                         authorProfileDialogFragment.show(getSupportFragmentManager(), "AuthorProfileDialogFragment");
                         break;
-                    }
-                    case("timeout"): {
-                        removeLoadingCard();
                     }
                 }
             }
