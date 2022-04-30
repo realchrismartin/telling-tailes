58fa37f499a5b30d401382b7b8f25f08bb1ff105
CM <omegajms@gmail.com>
2022-04-30 18:38:52 -0400

    TT-143: various fixes

--- app/src/main/java/com/telling/tailes/util/GPTUtils.java
+++ app/src/main/java/com/telling/tailes/util/GPTUtils.java
@@ -9,10 +9,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
